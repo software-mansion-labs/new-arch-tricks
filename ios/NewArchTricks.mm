4c98b8cfa73cacca7e6d607a20a2c63ee1b3ea64
Jakub Piasecki <jakub.piasecki@swmansion.com>
2024-09-27 14:29:37 +0200

    Expose JSI bindings on iOS

--- ios/NewArchTricks.mm
+++ ios/NewArchTricks.mm
@@ -9,9 +9,14 @@
 #import <React/RCTUtils.h>
 #import <ReactCommon/CallInvoker.h>
 #import <ReactCommon/RCTTurboModule.h>
+#import <ReactCommon/RCTTurboModuleWithJSIBindings.h>
 #import <NewArchTricks/NewArchTricksCommitHook.h>
+#import <NewArchTricks/NewArchTricksJSI.h>
 
 #import <react/renderer/uimanager/primitives.h>
+
+@interface NewArchTricks () <RCTTurboModuleWithJSIBindings>
+@end
 
 @implementation NewArchTricks {
  __weak RCTSurfacePresenter* _surfacePresenter;
@@ -28,6 +33,11 @@ RCT_EXPORT_MODULE()
 - (void)install
 {
  commitHook_ = std::make_shared<newarchtricks::NewArchTricksCommitHook>(_surfacePresenter.scheduler.uiManager);
+}
+
+- (void)installJSIBindingsWithRuntime:(facebook::jsi::Runtime &)runtime
+{
+  newarchtricks::NewArchTricksJSI::install(runtime, commitHook_);
 }
 
 - (void)setSurfacePresenter:(id<RCTSurfacePresenterStub>)surfacePresenter
