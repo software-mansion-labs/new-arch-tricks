c57464c777d782d42c97ec1d4e639426fdbb96cc
Jakub Piasecki <jakub.piasecki@swmansion.com>
2024-09-27 09:49:23 +0200

    Implement iOS

--- ios/NewArchTricks.mm
+++ ios/NewArchTricks.mm
@@ -3,18 +3,36 @@
 #import <React/RCTBridge+Private.h>
 #import <React/RCTBridge.h>
 #import <React/RCTBridgeModule.h>
+#import <React/RCTScheduler.h>
+#import <React/RCTSurfacePresenter.h>
+#import <React/RCTSurfacePresenterStub.h>
 #import <React/RCTUtils.h>
 #import <ReactCommon/CallInvoker.h>
 #import <ReactCommon/RCTTurboModule.h>
+#import <NewArchTricks/NewArchTricksCommitHook.h>
 
 #import <react/renderer/uimanager/primitives.h>
 
-@implementation NewArchTricks
+@implementation NewArchTricks {
+ __weak RCTSurfacePresenter* _surfacePresenter;
+ std::shared_ptr<newarchtricks::NewArchTricksCommitHook> commitHook_;
+}
+
 RCT_EXPORT_MODULE()
 
 - (NSNumber *)multiply:(double)a b:(double)b
 {
     return [[NSNumber alloc] initWithDouble:a * b];
+}
+
+- (void)install
+{
+ commitHook_ = std::make_shared<newarchtricks::NewArchTricksCommitHook>(_surfacePresenter.scheduler.uiManager);
+}
+
+- (void)setSurfacePresenter:(id<RCTSurfacePresenterStub>)surfacePresenter
+{
+ _surfacePresenter = surfacePresenter;
 }
 
 - (std::shared_ptr<facebook::react::TurboModule>)getTurboModule:(const facebook::react::ObjCTurboModule::InitParams &)params
