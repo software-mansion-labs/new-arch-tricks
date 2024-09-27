b4533cd64d6756cedb41edd8c1034e76997148f6
Jakub Piasecki <jakub.piasecki@swmansion.com>
2024-09-27 14:34:33 +0200

    Auto register custom views with the commit hook

--- src/NewArchTricksView.tsx
+++ src/NewArchTricksView.tsx
@@ -1,3 +1,43 @@
+import React from 'react';
 import NewArchTricksNativeComponent from './specs/NewArchTricksNativeComponent';
+import type { NativeProps } from './specs/NewArchTricksNativeComponent';
+// @ts-ignore this is not exposed
+import ReactNativeInterface from 'react-native/Libraries/ReactPrivate/ReactNativePrivateInterface';
 
-export { NewArchTricksNativeComponent as NewArchTricksView };
+export const NewArchTricksView = React.forwardRef<any, NativeProps>(
+  (props, forwardedRef) => {
+    const localRef = React.useRef<any>(null);
+    const [_registered, setRegistered] = React.useState(false);
+
+    return (
+      <NewArchTricksNativeComponent
+        {...props}
+        ref={(ref) => {
+          if (ref) {
+            const node = ReactNativeInterface.getNodeFromPublicInstance(ref);
+            // @ts-ignore injected via JSI
+            global.__NAT_registerNode(node);
+
+            localRef.current = ref;
+            setRegistered(true);
+          } else {
+            const node = ReactNativeInterface.getNodeFromPublicInstance(
+              localRef.current
+            );
+            // @ts-ignore injected via JSI
+            global.__NAT_unregisterNode(node);
+            localRef.current = null;
+          }
+
+          if (forwardedRef) {
+            if (typeof forwardedRef === 'function') {
+              forwardedRef(ref);
+            } else {
+              (forwardedRef as React.MutableRefObject<any>).current = ref;
+            }
+          }
+        }}
+      />
+    );
+  }
+);
--- src/specs/NewArchTricksNativeComponent.ts
+++ src/specs/NewArchTricksNativeComponent.ts
@@ -1,7 +1,7 @@
 import codegenNativeComponent from 'react-native/Libraries/Utilities/codegenNativeComponent';
 import type { ViewProps } from 'react-native';
 
-interface NativeProps extends ViewProps {
+export interface NativeProps extends ViewProps {
   color: string;
 }
 
